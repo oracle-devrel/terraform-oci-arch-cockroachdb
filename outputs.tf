3549bfc32786f12e54774d149854fa8fc299e66c
Nuno Gonçalves <nuno.goncalves@oracle.com>
2022-09-16 10:01:14 +0200

    OL9 + LB fix

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 output "loadbalancer_public_url" {
-  value = "http://${oci_load_balancer_load_balancer.lb1.ip_addresses[0]}:8080"
+  value = "http://${oci_load_balancer_load_balancer.lb1.ip_address_details[0].ip_address}:8080"
 }
 
 output "generated_ssh_private_key" {
--- variables.tf
+++ variables.tf
@@ -70,7 +70,7 @@ variable "instance_os" {
 
 variable "linux_os_version" {
   description = "Operating system version for all Linux instances"
-  default     = "7.9"
+  default     = "9"
 }
 
 # Dictionary Locals
